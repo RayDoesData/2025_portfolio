a1e71241cbaf310b5e7aff33b08f4aa2c76d62e2
dfong108 <davidsonf108@gmail.com>
2025-04-25 11:43:03 -0400

    Projects
    build out a projects sections that is clean, responsive, and uses static data to render a beautiful ui

--- src/components/ProjectDetails.jsx
+++ src/components/ProjectDetails.jsx
@@ -1,4 +1,5 @@
 import { img } from "motion/react-client";
+import { motion } from "motion/react";
 import React from "react";
 
 const ProjectDetails = ({
@@ -12,12 +13,16 @@ const ProjectDetails = ({
 }) => {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center w-full h-full overflow-hidden backdrop-blur-sm">
-      <div className="relative max-w-2xl border shadow-sm rounded-2xl bg-gradient-to-l from-midnight to-navy border-white/10">
+      <motion.div
+        className="relative max-w-2xl border shadow-sm rounded-2xl bg-gradient-to-l from-midnight to-navy border-white/10"
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1, }}
+      >
         <button
           onClick={closeModal}
           className="absolute p-2 rounded-sm top-5 right-5 bg-midnight hover:bg-gray-500 cursor-pointer"
         >
-            <img src="assets/close.svg" className="w-6 h-6" />
+          <img src="assets/close.svg" className="w-6 h-6" />
         </button>
         <img src={image} alt="" className="w-full rounded-t-2xl" />
         <div className="p-5">
@@ -47,7 +52,7 @@ const ProjectDetails = ({
             </a>
           </div>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
