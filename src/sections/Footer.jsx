b902de51efa11367565377fcbf2dbbc121277fc8
RayDoesData <raydoesdata@gmail.com>
2025-06-10 17:31:41 -0400

    Updated socials

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -6,38 +6,37 @@ import { FaGithub } from "react-icons/fa";
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   const siteName = import.meta.env.VITE_MY_NAME;
+
   return (
-    <section className="flex flex-wrap items-center justify-between gap-5 pb-3 text sm text-neutral-400 c-space">
+    <section className="flex flex-wrap items-center justify-between gap-5 pb-3 text-sm text-neutral-400 c-space">
       <Divider />
       <div className="flex gap-2">
         <p>Terms & Conditions</p>
         <p>|</p>
-        <p> Privacy Policy</p>
+        <p>Privacy Policy</p>
       </div>
-      <div className="flex gap-3 items-center ">
-     <div className="flex gap-3 items-center">
-  <a 
-    href="https://github.com/RayDoesData/portfolio" 
-    target="_blank" 
-    rel="noopener noreferrer"
-  >
-    <FaGithub className="w-6 h-6 text-white hover:scale-110 transition-all duration-500 ease-in-out" />
-  </a>
-  {mySocials?.map((social, index) => (
-    <a href={social.href} key={index} target="_blank">
-      <img 
-        src={social.icon} 
-        className="w-5 h-5 hover:scale-110 transition-all duration-500 ease-in-out" 
-        alt={social.name} 
-      />
-    </a>
-  ))}
-</div>
-      <p className="">
-        © {currentYear} {siteName} All Rights Reserved
-      </p>
+      <div className="flex gap-3 items-center">
+        <a
+          href="https://github.com/RayDoesData/portfolio"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub className="w-6 h-6 text-white hover:scale-110 transition-all duration-500 ease-in-out" />
+        </a>
+        {mySocials?.map((social, index) => (
+          <a href={social.href} key={index} target="_blank">
+            <img
+              src={social.icon}
+              className="w-5 h-5 hover:scale-110 transition-all duration-500 ease-in-out"
+              alt={social.name}
+            />
+          </a>
+        ))}
+      </div>
+      <p>© {currentYear} {siteName} All Rights Reserved</p>
     </section>
   );
 };
 
 export default Footer;
+
