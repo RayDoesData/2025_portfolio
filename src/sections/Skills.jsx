9dc582d65797b2e273fa49cfad274a78065a7bb9
RayDoesData <raydoesdata@gmail.com>
2025-06-11 10:58:06 -0400

    Updated skills

--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -37,7 +37,7 @@ const SkillsCard = ({ name, icon}) => {
 export default function Skills() {
   return (
     <div className="items-start mt-25 md:mt-35 c-space">
-    <h2 className="text-heading">Pro Skills & Tools</h2>
+    <h2 className="text-heading">Skills & Tools</h2>
 
       <div className="relative flex w-full flex-col items-center justify-center overflow-hidden mt-12">
         <Marquee pauseOnHover className="[--duration:20s]">
